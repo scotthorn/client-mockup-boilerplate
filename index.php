9b707aa6ff97aed216000f9b6c8da1fb136f7e46
Scott Horn <scott.horn@achieveinternet.com>
2017-07-02 00:22:02 -0400

    Initial work for wte0 launch.

--- index.php
+++ index.php
@@ -13,21 +13,33 @@
     <div class="content-wrapper table-row">
       <div class="middle table-cell">
         <h1><a href="/"><span>Evan</span> <span>Winters</span></a></h1>  
-        <ul class="buy-links">
-          <li><h2>The Witch of Bracken's Hollow</h2></li>
-          <li>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-              <input type="hidden" name="cmd" value="_s-xclick">
-              <input type="hidden" name="hosted_button_id" value="GWHKWZNPL36XN">
-              <input type="submit" value="Order with PayPal">
-              <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        <div class="buy-links">
+          <div class="image"><img src="awt00.jpg" alt="A Winter's Tale, Issue Zero, Cover"></div>
+          <div class="content">
+            <h2>A Winter's Tale, Issue Zero</h2>
+            <div class="free">Free with your subscription to our newsletter. <a href="#writing">Details</a></div>
+            
+            <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+            <form action="//evanwinters.us11.list-manage.com/subscribe/post?u=a7529490683f9c5c822dc89d6&amp;id=d8b66731b8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+              
+            <div class="mc-field-group">
+              <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="email address">
+            </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;"><input type="text" name="b_a7529490683f9c5c822dc89d6_d8b66731b8" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
             </form>
-            <div class="comment">* Signed Paperback</div>
-          </li>
-          <li><a href="http://amzn.to/1WQoMzc">
-            <i class="fa fa-amazon"></i> Available on Amazon
-          </a><div class="comment">* Digital/Paperback</div></li>
-        </ul>
+            </div>
+
+            <!--End mc_embed_signup-->
+          </div>
+        </div>
         <nav id="main-nav">
           <ul class="menu">
             <li><a href="#writing">Writing</a></li>
@@ -49,6 +61,34 @@
       <a href="#header" class="top-link" title="back to top"><i class="fa fa-chevron-up"></i></a>
       <h2>Writing</h2>
       <ul>
+        <li>
+          <img src="awt00.jpg" alt="A Winter's Tale, Issue Zero, Cover" class="book-image">
+          <h3>A Winter's Tale, Issue Zero</h3>
+          <p>Sign up of our email newsletter to receive links to free downloads for this graphic novel, in both PDF and comic book archive files.</p>
+          <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+              <form action="//evanwinters.us11.list-manage.com/subscribe/post?u=a7529490683f9c5c822dc89d6&amp;id=d8b66731b8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+                
+              <div class="mc-field-group">
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="email address">
+              </div>
+                <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none"></div>
+                  <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;"><input type="text" name="b_a7529490683f9c5c822dc89d6_d8b66731b8" tabindex="-1" value=""></div>
+                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                  </div>
+              </form>
+            </div>
+
+            <!--End mc_embed_signup-->
+            <p class="note"><em>* Your email stays private, and will not be sold or shared with anyone. Unsubscribe anytime.</em></p>
+            <p>After a long search for her lost sister, Anna learned the grisly truth and will have her justice.</p>
+            <p>Her captors may think they have her under control, but Anna is determined not to let this be the end of the family story.</p>
+
+        </li>
         <li>
           <a href="http://amzn.to/1WQpnAT" title="View this book on Amazon.com"><img class="book-image" src="witch.jpg" alt="The Witch of Bracken's Hollow"></a>
           <h3>The Witch of Bracken's Hollow</h3>
